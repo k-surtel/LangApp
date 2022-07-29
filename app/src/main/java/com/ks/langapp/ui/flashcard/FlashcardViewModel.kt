a74084174509055cf8b28a1c6329b83cceb1f88b
kasiasurtel <surtel.katarzyna@gmail.com>
2022-07-29 21:56:09 +0200

    Flashcards screen update

--- app/src/main/java/com/ks/langapp/ui/flashcard/FlashcardViewModel.kt
+++ app/src/main/java/com/ks/langapp/ui/flashcard/FlashcardViewModel.kt
@@ -1,9 +1,13 @@
 package com.ks.langapp.ui.flashcard
 
+import android.view.View
 import androidx.lifecycle.*
 import com.ks.langapp.data.database.entities.Card
 import com.ks.langapp.data.repository.LangRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,20 +17,23 @@ class FlashcardViewModel @Inject constructor(
     private val repository: LangRepository
     ) : ViewModel() {
 
-    lateinit var cards: List<Card>
+    private val  _cards = MutableStateFlow<List<Card>>(listOf())
+    val cards: StateFlow<List<Card>> = _cards
 
-    private val _currentIndex = MutableLiveData<Int>()
-    private val currentIndex: LiveData<Int> get() = _currentIndex
+    private val _backVisibility = MutableStateFlow(View.GONE)
+    val backVisibility: StateFlow<Int> = _backVisibility
 
-    private val _currentWord = MutableLiveData<String>()
-    val currentWord: LiveData<String> get() = _currentWord
+    private var _currentIndex = MutableStateFlow(0)
+    val currentIndex: StateFlow<Int> = _currentIndex
 
+    private val _currentCard = MutableStateFlow<Card?>(null)
+    val currentCard: StateFlow<Card?> = _currentCard
 
     fun processArguments(deckId: Long) {
         if(deckId != Long.MIN_VALUE) {
             viewModelScope.launch {
                 repository.getCardsOfADeck(deckId).collectLatest {
-                    cards = it.shuffled()
+                    _cards.value = it.shuffled()
 
                     _currentIndex.value = 0
                     setFlashcard()
@@ -35,19 +42,27 @@ class FlashcardViewModel @Inject constructor(
         }
     }
 
-    fun setFlashcard() { _currentWord.value = cards[currentIndex.value!!].front }
+    private fun setFlashcard() {
+        val shouldDelay = backVisibility.value == View.VISIBLE
+
+        if (shouldDelay) _backVisibility.value = View.GONE
+        viewModelScope.launch {
+            if (shouldDelay) delay(610)
+            _currentCard.value = cards.value[_currentIndex.value]
+        }
+    }
 
     fun onCardClicked() {
-        if(currentWord.value == cards[currentIndex.value!!].front)
-            _currentWord.value = cards[currentIndex.value!!].back
-        else
-            _currentWord.value = cards[currentIndex.value!!].front
+        if (_backVisibility.value == View.GONE) _backVisibility.value = View.VISIBLE
+        else _backVisibility.value = View.GONE
     }
 
     fun onWrongClicked() {
-        if(cards.size > currentIndex.value!!.plus(1))
-            _currentIndex.value = _currentIndex.value!!.plus(1)
-        else _currentIndex.value = 0
+        if(cards.value.size > _currentIndex.value + 1) _currentIndex.value++
+        else {
+            _currentIndex.value = 0
+            _cards.value = _cards.value.shuffled()
+        }
         setFlashcard()
     }
 }
