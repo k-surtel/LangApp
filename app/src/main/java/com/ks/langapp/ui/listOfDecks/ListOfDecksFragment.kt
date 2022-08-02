5d4dccd2bc9ecc9927f81f37e4a6338d25563480
kasiasurtel <surtel.katarzyna@gmail.com>
2022-08-02 21:27:37 +0200

    Fab animation pt. 1

--- app/src/main/java/com/ks/langapp/ui/listOfDecks/ListOfDecksFragment.kt
+++ app/src/main/java/com/ks/langapp/ui/listOfDecks/ListOfDecksFragment.kt
@@ -1,19 +1,21 @@
 package com.ks.langapp.ui.listOfDecks
 
 import android.app.AlertDialog
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.AnimationUtils.loadAnimation
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.ks.langapp.R
-import com.ks.langapp.data.database.LangDatabase
 import com.ks.langapp.data.database.entities.Deck
 import com.ks.langapp.databinding.FragmentListOfDecksBinding
 import com.ks.langapp.ui.adapters.DecksAdapter
@@ -28,6 +30,12 @@ class ListOfDecksFragment : Fragment() {
 
     private lateinit var binding: FragmentListOfDecksBinding
     private val viewModel: ListOfDecksViewModel by viewModels()
+
+    private val fabOpenRotation: Animation by lazy { loadAnimation(requireContext(), R.anim.fab_open_rotate) }
+    private val fabCloseRotation: Animation by lazy { loadAnimation(requireContext(), R.anim.fab_close_rotate) }
+    private val fabAppear: Animation by lazy { loadAnimation(requireContext(), R.anim.fab_appear) }
+    private val fabDisappear: Animation by lazy { loadAnimation(requireContext(), R.anim.fab_disappear) }
+    private var fabMenuOpened = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View {
@@ -45,6 +53,37 @@ class ListOfDecksFragment : Fragment() {
         })
         lifecycleScope.launch { viewModel.decks.collectLatest { adapter.submitList(it) } }
         binding.decks.adapter = adapter
+
+        fabAppear.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                binding.fabAdd.visibility = View.INVISIBLE
+                binding.fabAddLabel.visibility = View.INVISIBLE
+                binding.fabImport.visibility = View.INVISIBLE
+                binding.fabImportLabel.visibility = View.INVISIBLE
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+        })
+
+        fabDisappear.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.fabAdd.visibility = View.GONE
+                binding.fabAddLabel.visibility = View.GONE
+                binding.fabImport.visibility = View.GONE
+                binding.fabImportLabel.visibility = View.GONE
+            }
+
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+        })
+
+        binding.fab.setOnClickListener {
+            onFabClick()
+        }
 
         return binding.root
     }
@@ -69,6 +108,24 @@ class ListOfDecksFragment : Fragment() {
         }
         val dialog = builder.create()
         dialog.show()
+    }
+
+    private fun onFabClick() {
+        if (fabMenuOpened) {
+            binding.fab.startAnimation(fabCloseRotation)
+            binding.fabAdd.startAnimation(fabDisappear)
+            binding.fabAddLabel.startAnimation(fabDisappear)
+            binding.fabImport.startAnimation(fabDisappear)
+            binding.fabImportLabel.startAnimation(fabDisappear)
+        } else {
+            binding.fab.startAnimation(fabOpenRotation)
+            binding.fabAdd.startAnimation(fabAppear)
+            binding.fabAddLabel.startAnimation(fabAppear)
+            binding.fabImport.startAnimation(fabAppear)
+            binding.fabImportLabel.startAnimation(fabAppear)
+        }
+
+        fabMenuOpened = !fabMenuOpened
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
